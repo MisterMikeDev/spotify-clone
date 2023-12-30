f09dd07019e6b3a04205b1c778e9ce4e86dd5742
MrMikeDev <97488618+MisterMikeDev@users.noreply.github.com>
2023-12-30 16:23:42 -0600

    :fire: Update layout classes and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        backgroundSpotify: "#000",
-        textSpotify: "#b3b3b3",
-        containerSpotify: "#121212",
-        containerSpotifyOther: "#181818",
-        greenSpotify: "#1fdf64",
-        tagSpotify: "hsla(0,0%,100%,.1)",
-      },
+module.exports = {
+    content: ["./src/**/*.{astro,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                backgroundSpotify: "#000",
+                textSpotify: "#b3b3b3",
+                containerSpotify: "#121212",
+                containerSpotifyOther: "#181818",
+                greenSpotify: "#1fdf64",
+                tagSpotify: "hsla(0,0%,100%,.1)"
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
